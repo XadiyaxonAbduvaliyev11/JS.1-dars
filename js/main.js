4d01eb651eb7faec67c6be61c7e5eb9dcbfed9aa
aqdtjjksytjksfyksk <134036555+AnvarovFarxod@users.noreply.github.com>
2023-05-31 16:37:21 +0500

    Bu JS(Java Script) kodlari
    Yuklandi

---
+++ js/main.js
@@ -0,0 +1,75 @@
+/*console.log(4+4*4/4-4)*/
+/*alert("Saytimizga hush kelibsiz!\")*/
+/*document.write("Men bodyda ko'rinaman!")*/
+/*let x=2
+let y=5
+x=10
+let  x=2,y=5,z=10
+ */
+
+/*let color="#f1f"*/
+/*let salom="Assalomu alaykum!"*/
+
+/*document.write(salom)*/
+/* data types:number,string*/
+
+/*document.write(x+y+z)*/
+
+/*
+let x=5
+let soz="JavaScript"
+document.write(typeof soz)
+*/
+
+
+/*JS arifmetik hisoblashlar*/
+
+let x=5
+/*x+=5*//*x=5+5*/;
+/*x-=2*//*x=5-2*/
+/*x/=2*/
+
+/*let qoldiq= x%4;
+let daraja= x**25;*/
+/*++x;*/
+/*--x
+--x*/
+/*x++
+x--*/
+
+/*document.write(x+"5")*/
+
+/*if ("Salom"=="Salom"){
+    console.log("Ular teng!");
+}
+else{
+    console.log("Ular teng emas!")
+    }*/
+
+/*if=>agar*/
+/*else if=>yoki bo'lmasa*/
+/*else if=>u holda*/
+
+if (45 == 46){
+    console.log("Bu son 45ga teng!");
+}
+else if (45 == 45){
+    console.log("Endi bu son 45ga teng!");
+}
+else{
+    console.log("Ikkala son ham 45ga teng emas!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/main2.js
@@ -0,0 +1,54 @@
+if (2011 == 2010){
+    console.log("Bu son 2011ga teng!");
+}
+else if (2011 == 2021){
+    console.log("Endi bu son 2011ga teng!");
+}
+else{
+    console.log("Ikkala son ham 2011ga teng emas!")
+}
+
+
+if (1981 == 1982){
+    console.log("Bu son 1981dan katta!");
+}
+else if (1981 == 1982){
+    console.log("Endi bu son 1981dan katta!");
+}
+else{
+    console.log("Ikkala son ham 1981dan katta emas!")
+}
+
+
+if (2011 == 2010){
+    console.log("Bu son 2011ga teng!");
+}
+else if (2011 == 2011){
+    console.log("Endi bu son 2011ga teng!");
+}
+else{
+    console.log("Ikkala son ham 2011ga teng emas!")
+}
+
+
+if (11 == 12){
+    console.log("Bu sonlarni qo'shganda 20ga teng!");
+}
+else if (11 == 21){
+    console.log("Endi bu sonlarni qo'shganda 31ga teng!");
+}
+else{
+    console.log("Ikkala son ham 25ga teng emas!")
+}
+
+
+if (2011 == 2010){
+    console.log("Bu son 2011ga teng!");
+}
+else if (2011 == 2011){
+    console.log("Endi bu son 2011ga teng!");
+}
+else{
+    console.log("Ikkala son ham 2011ga teng emas!")
+}
+
